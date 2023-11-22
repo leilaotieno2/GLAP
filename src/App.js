1222e9462dd9ba56a893b7ef0f8115b1ac85e5ce
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-11-22 14:43:14 +0300

    landing

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignIn from './Signin';
+import Landing from './Landing';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<SignIn />} />
+        <Route path="/home" element={<Landing />} />
         {/* Define other routes here */}
       </Routes>
     </Router>
