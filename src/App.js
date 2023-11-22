a4caabdb4ef81ffac7b894a0dcea05f938663550
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-11-22 18:39:45 +0300

    Sigin

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignIn from './Signin';
 import Landing from './Landing';
-
+import Student from './Student';
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<SignIn />} />
         <Route path="/home" element={<Landing />} />
+        <Route path="/student" element={<Student />} />
         {/* Define other routes here */}
       </Routes>
     </Router>
