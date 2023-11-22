a7023dd6476b0e9e82a8724c3b67b0a442c648f8
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-11-23 01:21:57 +0300

    Sigin

--- src/Landing.js
+++ src/Landing.js
@@ -1,96 +1,61 @@
+// Landing.jsx
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Navbar, Nav } from 'react-bootstrap'; // Import Navbar and Nav components
+import './Landing.css';
 
 const Landing = () => {
   return (
-    <div className="w-full">
-      <nav className="bg-white shadow-lg">
-        <div className="md:flex items-center justify-between py-2 px-8 md:px-12">
-          <div className="flex justify-between items-center">
-            <div className="text-2xl font-bold text-gray-800 md:text-3xl">
-              <a href="#">Brand</a>
-            </div>
-            <div className="md:hidden">
-              <button
-                type="button"
-                className="block text-gray-800 hover:text-gray-700 focus:text-gray-700 focus:outline-none"
-              >
-                <svg
-                  className="h-6 w-6 fill-current"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    className="hidden"
-                    d="M16.24 14.83a1 1 0 0 1-1.41 1.41L12 13.41l-2.83 2.83a1 1 0 0 1-1.41-1.41L10.59 12 7.76 9.17a1 1 0 0 1 1.41-1.41L12 10.59l2.83-2.83a1 1 0 0 1 1.41 1.41L13.41 12l2.83 2.83z"
-                  />
-                  <path d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z" />
-                </svg>
-              </button>
-            </div>
-          </div>
-          <div className="flex flex-col md:flex-row hidden md:block -mx-2">
-            <a
-              href="#"
-              className="text-gray-800 rounded hover:bg-gray-900 hover:text-gray-100 hover:font-medium py-2 px-2 md:mx-2"
-            >
-              Home
-            </a>
-            <a
-              href="#"
-              className="text-gray-800 rounded hover:bg-gray-900 hover:text-gray-100 hover:font-medium py-2 px-2 md:mx-2"
-            >
-              About
-            </a>
-            <a
-              href="#"
-              className="text-gray-800 rounded hover:bg-gray-900 hover:text-gray-100 hover:font-medium py-2 px-2 md:mx-2"
-            >
-              Contact
-            </a>
-          </div>
-        </div>
-      </nav>
-      <div className="flex bg-white" style={{ height: '600px' }}>
-        <div className="flex items-center text-center lg:text-left px-8 md:px-12 lg:w-1/2">
-          <div>
-            <h2 className="text-3xl font-semibold text-gray-800 md:text-4xl">
-              Build Your New <span className="text-indigo-600">Idea</span>
-            </h2>
-            <p className="mt-2 text-sm text-gray-500 md:text-base">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Blanditiis commodi cum cupiditate ducimus, fugit harum id
-              necessitatibus odio quam quasi, quibusdam rem tempora voluptates.
-              Cumque debitis dignissimos id quam vel!
-            </p>
-            <div className="flex justify-center lg:justify-start mt-6">
-              <a
-                className="px-4 py-3 bg-gray-900 text-gray-200 text-xs font-semibold rounded hover:bg-gray-800"
-                href="#"
-              >
-                Get Started
-              </a>
-              <a
-                className="mx-4 px-4 py-3 bg-gray-300 text-gray-900 text-xs font-semibold rounded hover:bg-gray-400"
-                href="#"
-              >
-                Learn More
-              </a>
+    <div>
+      {/* Header */}
+      <header className="header">
+        <h1 className="site-name">GLAP</h1>
+      </header>
+
+      {/* Navigation Bar */}
+      <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
+        <Navbar.Toggle aria-controls="navbar-nav" />
+        <Navbar.Collapse id="navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/teachers">Teachers</Nav.Link>
+            <Nav.Link as={Link} to="/events">Events</Nav.Link>
+            <Nav.Link as={Link} to="/about">About Us</Nav.Link>
+            <Nav.Link as={Link} to="/faq">FAQ</Nav.Link>
+            <Nav.Link as={Link} to="/admission">Online Admission</Nav.Link>
+            <Nav.Link as={Link} to="/contact">Contact Us</Nav.Link>
+            <Nav.Link as={Link} to="/login">Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
+      {/* Custom Carousel */}
+      <div id="customCarousel" className="carousel slide" data-bs-ride="carousel">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="d-block w-100" alt="Carousel Slide 1" />
+            <div className="carousel-caption text-start">
+              <h5 className="text-dark">Slide 1 Title</h5>
+              <p className="text-dark">Slide 1 Description</p>
             </div>
           </div>
-        </div>
-        <div
-          className="hidden lg:block lg:w-1/2"
-          style={{ clipPath: 'polygon(10% 0, 100% 0%, 100% 100%, 0 100%)' }}
-        >
-          <div
-            className="h-full object-cover"
-            style={{
-              backgroundImage:
-                'url(https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1352&q=80)',
-            }}
-          >
-            <div className="h-full bg-black opacity-25"></div>
+          <div className="carousel-item">
+            <img src="https://images.unsplash.com/photo-1542810634-71277d95dcbb?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="d-block w-100" alt="Carousel Slide 2" />
+            <div className="carousel-caption text-start">
+              <h5 className="text-dark">Slide 2 Title</h5>
+              <p className="text-dark">Slide 2 Description</p>
+            </div>
           </div>
+          {/* Add more carousel items as needed */}
         </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#customCarousel" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#customCarousel" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
       </div>
     </div>
   );
